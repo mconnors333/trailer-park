c2344e1d954b62c1384f2099d7f9a15b1f3482e4
mconnors333 <matthew.connors333@gmail.com>
2017-02-13 13:07:59 -0500

    working this project

---
+++ js/ClientApp.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import store from './store'
+import { render } from 'react-dom'
+import { BrowserRouter, Match } from 'react-router'
+import { Provider } from 'react-redux' // makes redux easier
+import Landing from './Landing'
+import Search from './Search'
+import Details from './Details'
+import axios from 'axios'
+import Add from './Add'
+import '../public/normalize.css'
+import '../public/style.css'
+
+// let data = axios.get(`https://mattflix-8387b.firebaseio.com/shows/.json`)
+//
+// data = data.then( (response) => {
+//   response.data
+// })
+// console.log(data)
+
+class App extends React.Component {
+  constructor () {
+    super ();
+    this.state = {
+      shows: []
+    }
+  }
+  componentDidMount () {
+    var _this = this
+    this.serverRequest =
+      axios
+        .get('https://mattflix-8387b.firebaseio.com/shows/.json')
+        .then(function (result) {
+          _this.setState({
+            shows: result.data
+          })
+        })
+  }
+  render () {
+    return (
+      <BrowserRouter>
+        <Provider store={store}>
+          <div className='app'>
+            <Match exactly pattern='/' component={Landing} />
+            <Match exactly pattern='/add' component={Add} />
+            <Match
+              pattern='/search'
+              component={(props) => <Search shows={this.state.shows} {...props} />}
+            />
+            <Match
+              pattern='/details/:id'
+              component={(props) => {
+                const shows = this.state.shows.filter((show) => props.params.id === show.imdbID)
+                return <Details show={shows[0]} {...props} />
+              }}
+              />
+          </div>
+        </Provider>
+      </BrowserRouter>
+    )
+  }
+}
+
+render(<App />, document.getElementById('app'))
